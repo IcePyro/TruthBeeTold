77796334a74272bb0c84de5b3ef6a9fbdf93ba95
Paul Methfessel <paul.methfessel@web.de>
2021-07-16 18:28:48 +0200

    Add two folder setup

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/app.sass';
+import React from 'react';
+
+export default class App extends React.Component {
+  render(): React.ReactNode {
+    return (
+        <h1>App</h1>
+    );
+  }
+}
