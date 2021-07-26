ae787a4d164b8803ad984af78d4eb38f37918c79
Paul Methfessel <paul.methfessel@web.de>
2021-07-26 21:41:50 +0200

    Use https on production

--- server/session.ts
+++ server/session.ts
@@ -1,19 +1,31 @@
 import {Server} from 'socket.io';
 import * as express from 'express';
 import * as http from 'http';
+import * as https from 'https';
+import {Express} from 'express';
+import * as fs from 'fs';
 
 require("dotenv").config();
 
-/*const options = {
-    key: fs.readFileSync(process.env.KEYLOCATION),
-    cert: fs.readFileSync(process.env.CERTLOCATION)
-}*/
 
+function createServerDevelop(app: Express) {
+    return http.createServer(app);
+}
+
+function createServerProduction(app: Express) {
+    const options = {
+        key: fs.readFileSync(process.env.KEYLOCATION),
+        cert: fs.readFileSync(process.env.CERTLOCATION)
+    };
+    return https.createServer(options, app);
+}
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 function init(): Server {
     const port = parseInt(process.env.PORT || '3000');
     const app = express();
-    const server = http.createServer(app);
+    const server = isProduction ? createServerProduction(app) : createServerDevelop(app);
     const io = new Server(server, {
         cors: {
             origin: process.env.CLIENT || 'http://localhost:1234'
@@ -23,10 +35,5 @@ function init(): Server {
     console.log('Server is running');
     return io;
 }
-
-
-//const server = https.createServer(options, app)
-//console.log("options-key:" + options.key)
-//console.log("key: " + process.env.KEYLOCATION)
 
 export const io = init();
