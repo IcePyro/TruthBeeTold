d02580b77529a35d34c1ca57803f71945903ff19
Louis Haub <louis.haub@googlemail.com>
2021-07-26 19:19:42 +0200

    clean up imports

--- server/States/article-select.ts
+++ server/States/article-select.ts
@@ -1,12 +1,8 @@
-import {StateID} from "./state-updater";
+import {StateID, updateMultipleUserStates} from "./state-updater";
 import {io} from '../session';
 import Room from '../types/Room';
 import {activeUsers, User} from '../types/User';
 
-const helper = require('../helper-functions')
-const stateNameToID = require("./state-updater").stateNameToID
-const updateUserState = require("./state-updater").updateUserState
-const stateUpdater = require("./state-updater")
 
 exports.init = function (_, user: User){
     const room = user.room;
@@ -15,9 +11,9 @@ exports.init = function (_, user: User){
         if(articleID){
             user.articleID = articleID;
             if(user.room.allArticleLock){
-                junctionUsers(io, activeUsers, room)
+                junctionUsers(io, room)
             } else {
-                room.emitAllWithout(user, 'lockedinarticle', userID);
+                room.emitAllWithout(user, 'lockedinarticle', user.id);
             }
         }
     }
@@ -34,7 +30,7 @@ function junctionUsers(io, room: Room){
     room.usersWithout([queen, bee]).forEach((curr) => {
         curr.state = StateID.Wasp;
     })
-    stateUpdater.updateMultipleUserStates(io, room.users);
+    updateMultipleUserStates(io, room.users);
 }
 
 
