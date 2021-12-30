6e5111020ef63f33728fff26909bd3f914695c0e
Louis Haub <louis.haub@googlemail.com>
2021-12-30 13:09:18 +0100

    autouse newest cert and key in cert folder

--- server/session.ts
+++ server/session.ts
@@ -14,16 +14,14 @@ function createServerDevelop(app: Express) {
 }
 
 function createServerProduction(app: Express) {
-    const options = {
-        key: fs.readFileSync(process.env.KEYLOCATION),
-        cert: fs.readFileSync(process.env.CERTLOCATION)
-    };
+    const options = getCertOptions()
     return https.createServer(options, app);
 }
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 function init(): Server {
+    getCertOptions()
     const port = parseInt(process.env.PORT || '3000');
     const app = express();
     const server = isProduction ? createServerProduction(app) : createServerDevelop(app);
@@ -39,3 +37,23 @@ function init(): Server {
 }
 
 export const io = init();
+
+function getCertOptions() {
+
+    try{
+        const cert = fs.readdirSync('./').filter(
+            value => value.startsWith('fullchain')).sort().pop()
+        const key = fs.readdirSync('./').filter(
+            value => value.startsWith('privkey')).sort().pop()
+        return {
+            key:key,
+            cert:cert
+        }
+
+    }catch(e){
+        return {
+            key: fs.readFileSync(process.env.KEYLOCATION),
+            cert: fs.readFileSync(process.env.CERTLOCATION)
+        }
+    }
+}
